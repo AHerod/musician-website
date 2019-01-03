2bdfcf5af7219b40203baa52b2906a8362c7097b
AHerod <metalvege@gmail.com>
2019-01-03 11:09:12 +0100

    initital commit with setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './sass/style.scss';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1> hello world!</h1>
+                <div className="test">
+                    <h2>TEST</h2>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
